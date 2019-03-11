39a8c14372866a8baed9f40c1f1b5a718160ed9a
segwitt <abhish014@gmail.com>
2019-03-11 19:01:02 +0530

    added misc/modpow.cpp

---
+++ templates/misc/modpow.cpp
@@ -0,0 +1,51 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+#define ll long long
+
+const ll mod = 1e9 + 7;
+
+
+ll add ( ll x , ll y ) {
+
+	x %= mod;
+	y %= mod;
+	return ( x + y ) % mod;
+}
+
+ll mul(ll x, ll y) {
+	x %= mod;
+	y %= mod;
+	return ( x * y ) % mod; 
+}
+
+
+ll modpow(ll x , ll y) {
+
+	// x ^ y
+	ll res = 1;
+	while ( y ) {
+
+		if ( y & 1 )
+			res = mul( res , x );
+		x = mul ( x , x );
+		y >>= 1;
+
+	}
+
+	return res;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+
+	cout << modpow( 3, 4 ) << endl;
+
+
+	return 0;
+}
